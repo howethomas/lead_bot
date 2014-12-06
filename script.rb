f5380bc97cc584b4012f3534a9a0ca219e25d9b9
Gordon DuQuesnay <gduquesnay@gmail.com>
2014-12-06 19:18:29 +0000

    fix script

--- script.rb
+++ script.rb
@@ -15,6 +15,13 @@ def take_note(instructions, termination_char)
   additional_notes
 end
 
+def ask_and_take_note(note_field, note_collection_msg, note_help_msg, note_termination_char)
+  ask :wants_to_take_note, note_collection_msg, :as => :boolean
+  if wants_to_take_note then
+    record note_field, take_note(note_help_msg, note_termination_char)
+  end
+end
+
 
 conversation do
   record :src, @src
@@ -38,53 +45,44 @@ conversation do
 
   if AWAY
     say AWAY_MSG
-    if COLLECT_NOTE
-      ask :wants_to_take_note, NOTE_COLLECTION_MSG, :as => :boolean
-      if wants_to_take_note then
-        record :note, take_note(NOTE_HELP_MSG, NOTE_TERMINATION_CHAR)
-    end
-    say SIGNATURE_MSG if SIGNATURE_MSG.present?
-  else
-    say WELCOME_MSG
-
-    ask :name, "May we please have your name?", :as => :text if COLLECT_NAME
-
-    if COLLECT_ALTERNATE_CONTACT
-      ask :alt_contact_better, "Is there a better number to reach you at?(y/n)", :as => :boolean
-      if alt_contact_better then
-        ask :alt_contact, "What number would be better to contact you on?", :as => :text
-      end
-    end
-
-    if COLLECT_REASON
-      record :reason_for_contact, take_note("How can we help you? #{NOTE_HELP_MSG}", NOTE_TERMINATION_CHAR)
-    end
-
-    # Here is where  you might ask for name, reason and alt contact
-    script_response = human!(FINDME_MSG)
-    timed_out = script_response == "script_timed_out"
-
-    if timed_out then
-      say NOT_AT_DESK_MSG
-      ask :text_back, "Can we text you when we come back? (y/n)", :as => :boolean
-      if text_back then
-        say "Thank you. We will be with you as soon as we can."
-        begin
-          # loop this until the script_response does not time out
-          # scripts can end because they've been requested to, or they are too long hanging around.
-          script_response = human!("")
-        end until script_response != "script_timed_out"
-        else
-          if COLLECT_NOTE
-            ask :wants_to_take_note, NOTE_COLLECTION_MSG, :as => :boolean
-            if wants_to_take_note then
-              record :note, take_note(NOTE_HELP_MSG, NOTE_TERMINATION_CHAR)
-            end
-          end
-        end
-      end
-    end
-    say SIGNATURE_MSG if SIGNATURE_MSG.present?
+    ask_and_take_note(:note, NOTE_COLLECTION_MSG, NOTE_HELP_MSG, NOTE_TERMINATION_CHAR) if COLLECT_NOTE
+    say SIGNATURE_MSG unless SIGNATURE_MSG.empty?
+    return
   end
-end
 
+  say WELCOME_MSG
+
+  ask :name, "May we please have your name?", :as => :text if COLLECT_NAME
+
+  if COLLECT_ALTERNATE_CONTACT
+    ask :alt_contact_better, "Is there a better number to reach you at?(y/n)", :as => :boolean
+    if alt_contact_better then
+      ask :alt_contact, "What number would be better to contact you on?", :as => :text
+    end
+  end
+
+  if COLLECT_REASON
+    record :reason_for_contact, take_note("How can we help you? #{NOTE_HELP_MSG}", NOTE_TERMINATION_CHAR)
+  end
+
+  # Here is where  you might ask for name, reason and alt contact
+  script_response = human!(FINDME_MSG)
+  timed_out = script_response == "script_timed_out"
+
+  if timed_out then
+    say NOT_AT_DESK_MSG
+    ask :text_back, "Can we text you when we come back? (y/n)", :as => :boolean
+    if text_back then
+      say "Thank you. We will be with you as soon as we can."
+      begin
+        # loop this until the script_response does not time out
+        # scripts can end because they've been requested to, or they are too long hanging around.
+        script_response = human!("")
+      end until script_response != "script_timed_out"
+    else
+      ask_and_take_note(:note, NOTE_COLLECTION_MSG, NOTE_HELP_MSG, NOTE_TERMINATION_CHAR) if COLLECT_NOTE
+    end
+  end
+
+  say SIGNATURE_MSG unless SIGNATURE_MSG.empty?
+end
